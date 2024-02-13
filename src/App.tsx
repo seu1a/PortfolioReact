73c9509c60be1f65daae2300a93085f1f2387506
rheeseula <rheeseula@dgsw.hs.kr>
2024-02-13 21:45:20 +0900

    Feat :: 리액트 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import { Component } from "react";
+import { Route, Routes } from "react-router-dom";
+
+import Home from "./pages/Home";
+
 import "./App.css";
 
 class App extends Component {
   render() {
-    return <></>;
+    return (
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    );
   }
 }
 
