056d2b46309b31125603df960c1639c0270b3162
rheeseula <rheeseula@dgsw.hs.kr>
2024-02-13 21:45:04 +0900

    Feat :: Home 페이지 생성

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+import { Container } from "../styles/global";
+
+class Home extends Component {
+  render() {
+    return <Container></Container>;
+  }
+}
+
+export default Home;
