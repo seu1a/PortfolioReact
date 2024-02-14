048e37c3423509e929e02922db6dc35631da149c
rheeseula <rheeseula@dgsw.hs.kr>
2024-02-14 13:02:13 +0900

    Fix :: Home 페이지 컨테이너 변경

--- src/styles/global.tsx
+++ src/styles/global.tsx
@@ -1,7 +1,20 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  position: relative;
+
   width: 100vw;
   height: 100vh;
-  background-color: black;
+
+  background-color: white;
+`;
+
+export const VerticalAlignedContainer = styled.div`
+  position: relative;
+
+  width: 100vw;
+  height: 100vh;
+
+  display: flex;
+  align-items: center;
 `;
