fec63c129cbefc1ea665e2824fc4c5dab05a41bc
rheeseula <rheeseula@dgsw.hs.kr>
2024-02-14 13:10:22 +0900

    Fix :: ProfileBox 크기 조정

--- src/styles/Profile.tsx
+++ src/styles/Profile.tsx
@@ -4,7 +4,7 @@ export const ProfileBox = styled.div`
   position: relative;
   margin: 0 auto;
 
-  width: 500px;
-  height: 250px;
+  width: 650px;
+  height: 350px;
   background-color: black;
 `;
