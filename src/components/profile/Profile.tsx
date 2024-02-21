42dbaa20a3a5f895d203db31de889f076e72dc97
rheeseula <rheeseula@dgsw.hs.kr>
2024-02-21 19:30:33 +0900

    Feat :: ProfileBox 애니메이션 추가

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,12 +1,60 @@
-import React from "react";
-import { Component } from "react";
+import React, { createRef, Component, RefObject } from "react";
 
 import * as S from "../../styles/profile/profile";
 
-class Profile extends Component {
+interface ProfileState {
+  visibleCount: number;
+}
+
+class Profile extends Component<{}, ProfileState> {
+  private ProfileBoxRef: RefObject<HTMLDivElement>;
+
+  constructor(props: {}) {
+    super(props);
+    this.ProfileBoxRef = createRef();
+
+    this.state = {
+      visibleCount: 0,
+    };
+  }
+
+  offerScroll = () => {
+    const { top: ProfileBoxTop } =
+      this.ProfileBoxRef.current!.getBoundingClientRect();
+
+    const ProfileImageDivIsInViewport =
+      ProfileBoxTop >= 0 && ProfileBoxTop <= window.innerHeight;
+
+    if (ProfileImageDivIsInViewport) {
+      this.setState((prevState) => ({
+        visibleCount: prevState.visibleCount + 1,
+      }));
+    }
+
+    console.log(this.state.visibleCount);
+  };
+
+  componentDidMount(): void {
+    this.offerScroll();
+    window.addEventListener("scroll", this.offerScroll);
+  }
+
+  componentWillUnmount(): void {
+    window.removeEventListener("scroll", this.offerScroll);
+  }
+
   render() {
     return (
-      <S.ProfileBox>
+      <S.ProfileBox
+        ref={this.ProfileBoxRef}
+        style={{
+          opacity: this.state.visibleCount >= 1 ? 1 : 0,
+          transform: `translateY(${
+            this.state.visibleCount >= 1 ? "0" : "50px"
+          })`,
+          transition: "opacity 0.5s, transform 1.2s",
+        }}
+      >
         <S.ProfileImageDiv>
           <S.ProfileImage src="images/profile_img.jpg" />
         </S.ProfileImageDiv>
