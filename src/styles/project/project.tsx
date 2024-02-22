ac1fccb93b3d6e15560862d7f0de493223eea2a7
I'm not Seula <95991567+Sulicious@users.noreply.github.com>
2024-02-22 18:07:43 +0900

    Feat :: ProjectElement 추가

--- src/styles/project/project.tsx
+++ src/styles/project/project.tsx
@@ -4,10 +4,24 @@ export const ProjectBox = styled.div`
     position: relative;
     display: flex;
 
-    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 
     width: 100%;
     height: 350px;
 
     background-color: #363636;
+`
+
+export const ProjectElement = styled.div`
+    position: relative;
+
+    width: 450px;
+    height: 300px;
+    
+    margin: 15px;
+
+    background-color: white;
+
+    border-radius: 15px;
 `
