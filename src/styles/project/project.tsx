908e467f326b98df9e1423349cda1395cecca7fc
I'm not Seula <95991567+Sulicious@users.noreply.github.com>
2024-02-24 17:54:35 +0900

    Feat :: project 스타일 생성

---
+++ src/styles/project/project.tsx
@@ -0,0 +1,13 @@
+import { styled } from "styled-components";
+
+export const ProjectBox = styled.div`
+    position: relative;
+    display: flex;
+
+    flex-direction: column;
+
+    width: 100%;
+    height: 350px;
+
+    background-color: #363636;
+`
