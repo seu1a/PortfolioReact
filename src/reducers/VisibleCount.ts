aeb26f56194b0a6012a522dbb5b846ba9247e768
I'm not Seula <rheeseula@gmail.com>
2024-03-05 10:07:20 +0900

    Fix :: 스토어 상태 분리

--- src/reducers/VisibleCount.ts
+++ src/reducers/VisibleCount.ts
@@ -1,27 +1,44 @@
 const initialState = {
-  visibleCount: 0,
+  profileVisibleCount: 0,
+  projectVisibleCount: 0,
 };
 
-const SET_VISIBLE_COUNT = "SET_VISIBLE_COUNT" as const;
+const SET_PROFILE_VISIBLE_COUNT = "SET_PROFILE_VISIBLE_COUNT" as const;
+const SET_PROJECT_VISIBLE_COUNT = "SET_PROJECT_VISIBLE_COUNT" as const;
 
-export const set_visible_count = (count: number) => ({
-  type: SET_VISIBLE_COUNT,
+export const set_profile_visible_count = (count: number) => ({
+  type: SET_PROFILE_VISIBLE_COUNT,
   payload: {
     visibleCount: count,
   },
 });
 
+export const set_project_visible_count = (count: number) => ({
+  type: SET_PROJECT_VISIBLE_COUNT,
+  payload: {
+    visibleCount: count,
+  },
+});
+
+type Action =
+  | ReturnType<typeof set_profile_visible_count>
+  | ReturnType<typeof set_project_visible_count>;
+
 type State = {
-  visibleCount: number;
+  profileVisibleCount: number;
+  projectVisibleCount: number;
 };
 
 export default function setVisibleCount(
   state = initialState,
-  action: ReturnType<typeof set_visible_count>
+  action: Action
 ): State {
   switch (action.type) {
-    case "SET_VISIBLE_COUNT":
-      return { visibleCount: action.payload.visibleCount };
+    case "SET_PROFILE_VISIBLE_COUNT":
+      return { ...state, profileVisibleCount: action.payload.visibleCount };
+
+    case "SET_PROJECT_VISIBLE_COUNT":
+      return { ...state, projectVisibleCount: action.payload.visibleCount };
 
     default:
       return state;
